d9db626df3b83b39a7de91bf6094ba545751703a
Lasoloz <17884077+Lasoloz@users.noreply.github.com>
2023-12-05 15:57:10 +0200

    [feat] Add output handling examples

--- examples/src/output-forwarding.tsx
+++ examples/src/output-forwarding.tsx
@@ -20,10 +20,45 @@ const Example = () => {
     });
   };
 
+  const openStringOutputModal = () => {
+    openModal(NoInputStringOutputModal, {
+      onClose: data => {
+        setOutput(`<OUTPUT>: ${data}`);
+      }
+    });
+  };
+
+  const openObjectOutputModal = () => {
+    openModal(NoInputObjectOutputModal, {
+      onClose: data => {
+        setOutput(`<OUTPUT>: ${data.count}`);
+      }
+    });
+  };
+
+  const openCancelableModal = () => {
+    openModal(NoInputStringOutputModal, {
+      onClose: data => {
+        setOutput(`<OUTPUT>: ${data}`);
+      },
+      onCancel: () => {
+        setOutput("<LAST MODAL WAS CANCELED>");
+      }
+    });
+  };
+
+  const openModalWithoutHandler = () => {
+    // openModal(NoInputStringOutputModal); // FIXME: This is a bug!
+  };
+
   return (
     <div>
       <h2>Open modal with different output forwarding strategies</h2>
       <button onClick={openNoOutputModal}>Open no output modal</button>
+      <button onClick={openStringOutputModal}>Open string output modal</button>
+      <button onClick={openObjectOutputModal}>Open object output modal</button>
+      <button onClick={openCancelableModal}>Open cancelable string output modal</button>
+      <button onClick={openModalWithoutHandler}>Open modal without output handling</button>
       <p>Last modal output: {output()}</p>
     </div>
   );
@@ -65,6 +100,77 @@ const StringInputNoOutputModal = (props: ModalProps<string>) => {
       <p>Another simple modal with no output.</p>
       <p>Input: {props.input}</p>
       <button onClick={handleClose}>Close</button>
+    </SimpleModal>
+  );
+};
+
+const NoInputStringOutputModal = (props: ModalProps<undefined, string>) => {
+  const [data, setData] = createSignal("");
+  let ref: HTMLInputElement;
+
+  const handleClose = () => {
+    props.onClose(data());
+  };
+
+  const handleOpenStringModal = () => {
+    props.openForwarding(StringInputStringOutputModal, { input: data() });
+  };
+
+  const handleReopenSelf = () => {
+    // clear data, because component won't be recreated
+    setData("");
+    ref.value = "";
+    // TODO: Document ^this catch in the readme
+    props.openForwarding(NoInputStringOutputModal);
+  };
+
+  return (
+    <SimpleModal>
+      <p>Simple modal with string output</p>
+      <input type="text" value="" onInput={e => setData(e.target.value)} ref={ref!} />
+      <button onClick={handleOpenStringModal}>Open modal with input from above</button>
+      <button onClick={handleReopenSelf}>Reopen self</button>
+      <button onClick={handleClose}>Close submitting</button>
+    </SimpleModal>
+  );
+};
+
+const StringInputStringOutputModal = (props: ModalProps<string, string>) => {
+  const [data, setData] = createSignal(props.input);
+
+  const handleClose = () => {
+    props.onClose(data());
+  };
+
+  return (
+    <SimpleModal>
+      <p>Simple modal with string input and string output</p>
+      <p>Input was: {props.input.trim() === "" ? "<NOTHING>" : props.input}</p>
+      <input type="text" value={props.input} onInput={e => setData(e.target.value)} />
+      <button onClick={handleClose}>Close submitting</button>
+    </SimpleModal>
+  );
+};
+
+const NoInputObjectOutputModal = (props: ModalProps<undefined, { count: number }>) => {
+  const [data, setData] = createSignal(0);
+
+  const handleClose = () => {
+    props.onClose({ count: data() });
+  };
+
+  return (
+    <SimpleModal>
+      <p>Simple modal with object output</p>
+      <input
+        type="number"
+        value="0"
+        min="-10"
+        max="+10"
+        step="1"
+        onInput={e => setData(parseInt(e.target.value))}
+      />
+      <button onClick={handleClose}>Close submitting</button>
     </SimpleModal>
   );
 };
